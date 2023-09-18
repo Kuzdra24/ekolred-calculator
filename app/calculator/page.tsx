be6d775839371b7911795b50c6b73cbaf12d29d1
Patryk <patryk.kuzdrowski@gmail.com>
2023-09-18 22:16:03 +0200

    Basic routs - admin, calculator

---
+++ app/calculator/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-semibold text-teal-900">Calculator</h1>
+      
+    </main>
+  );
+}
